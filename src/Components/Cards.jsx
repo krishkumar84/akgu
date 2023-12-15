c95ef3cc3bb10131d2c87c2c8eb925639a1c1ce8
krish <sharmageeta847@gmail.com>
2023-12-15 19:13:36 +0530

    cards

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Cards() {
+  return (
+    <div className='flex flex-wrap gap-16 items-center justify-center pt-16 pb-28'>
+      <div className="yellow w-64 h-36 bg-yellow-400 rounded-2xl"></div>
+      <div className="blue w-64 h-36 bg-blue-500 rounded-2xl"></div>
+      <div className="red w-64 h-36 bg-red-500 rounded-2xl"></div>
+      <div className="green w-64 h-36 bg-black rounded-2xl"></div>
+    </div>
+  )
+}
+
+export default Cards
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from "../Components/Navbar"
 import Carousal from '../Components/Carousal'
+import Cards from '../Components/Cards'
 
 const Home = () => {
   return (
     <>
      <Navbar/>
      <Carousal/>
+     <Cards/>
     </>
   )
 }
