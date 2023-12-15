8bf0791d9dc77c5b00f9a2f14ae8a6179dfeb716
Harsh Yadav <hy945196@gmail.com>
2023-12-15 18:49:03 +0530

    Structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "../Components/Navbar"
+import Carousal from '../Components/Carousal'
+
+const Home = () => {
+  return (
+    <>
+     <Navbar/>
+     <Carousal/>
+    </>
+  )
+}
+
+export default Home
