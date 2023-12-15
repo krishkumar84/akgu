6339788159a16bd4984a1fec56eb2ddbe1cf2ad9
krish <sharmageeta847@gmail.com>
2023-12-15 22:10:32 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import './App.css'
-import Home from './pages/Home'
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Home2 from './pages/Home2';
 
 function App() {
- 
-
   return (
-    <>
-      <Home/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home2" element={<Home2 />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div>
@@ -22,9 +22,11 @@ function Navbar() {
       <li>
         <a href="/" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">Home</a>
       </li>
+      <Link to="/home2">
       <li>
-        <a href="/" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+        <a href="/" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home2</a>
       </li>
+      </Link>
       <li>
         <a href="/" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
       </li>
