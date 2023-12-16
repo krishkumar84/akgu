4008e5c83010e752d1f3cc6554a29f28bc8f6564
Harsh Yadav <hy945196@gmail.com>
2023-12-16 12:23:51 +0530

    Added Cards

---
+++ src/Components/CarousalCards.jsx
@@ -0,0 +1,192 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation, Pagination,Autoplay,Mousewheel} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import "./CSS/Carousel-cards.css"
+
+
+const CarousalCards = () => {
+      
+  return (
+    <>
+      <div className="main-carousel-cards ">
+        <h1 className='mb-[5vh] ml-[14vw] text-4xl font-bold'>Explore Our Online Degree Programs</h1>
+        <hr  className='mb-[9vh]'/>
+        <Swiper className='bg-black h-[100vh] pt-9 w-[100%] bg-gradient-to-tl from-black via-yellow-400 relative  overflow-hidden'
+            spaceBetween={70}
+          slidesPerView={3/2}
+          centeredSlides={true}
+          loop={true}
+          //   autoplay={{
+          //     delay: 2500,
+          //     // disableOnInteraction: false,
+          //   }}
+          cssMode={true}
+          sticky={false}
+          freeMode={{ enable: true }}
+          mousewheel={{
+            mousewheel: true,
+          }}
+          
+          navigation={true}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            640: {
+              slidesPerView: 1,
+            },
+          }}
+          modules={[Autoplay, Pagination, Navigation, Mousewheel]}
+        >
+          <SwiperSlide className='transition-transform transform hover:scale-105'>
+          <div className=" boxes h-[84vh] w-[100%] transition-transform transform hover:scale-105">
+              <img
+                src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+                alt="AirMax Pro"
+                className="z-0 h-[100%] w-[100%] object-cover"
+              />
+              <div className="absolute w-[100%] inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+              <div className="absolute bottom-4 left-4 text-left">
+                <h1 className="text-lg font-semibold text-white">Delba</h1>
+                <p className="mt-2 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi, debitis?
+                </p>
+                <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+                  View Profile &rarr;
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className='transition-transform transform hover:scale-105'>
+          <div className=" boxes h-[84vh] w-[100%] transition-transform transform hover:scale-105">
+              <img
+                src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+                alt="AirMax Pro"
+                className="z-0 h-[100%] w-[100%] object-cover"
+              />
+              <div className="absolute w-[100%] inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+              <div className="absolute bottom-4 left-4 text-left">
+                <h1 className="text-lg font-semibold text-white">Delba</h1>
+                <p className="mt-2 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi, debitis?
+                </p>
+                <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+                  View Profile &rarr;
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className='transition-transform transform hover:scale-105'>
+          <div className=" boxes h-[84vh] w-[100%] transition-transform transform hover:scale-105">
+              <img
+                src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+                alt="AirMax Pro"
+                className="z-0 h-[100%] w-[100%] object-cover"
+              />
+              <div className="absolute w-[100%] inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+              <div className="absolute bottom-4 left-4 text-left">
+                <h1 className="text-lg font-semibold text-white">Delba</h1>
+                <p className="mt-2 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi, debitis?
+                </p>
+                <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+                  View Profile &rarr;
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className='transition-transform transform hover:scale-105'>
+          <div className=" boxes h-[84vh] w-[100%] transition-transform transform hover:scale-105">
+              <img
+                src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+                alt="AirMax Pro"
+                className="z-0 h-[100%] w-[100%] object-cover"
+              />
+              <div className="absolute w-[100%] inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+              <div className="absolute bottom-4 left-4 text-left">
+                <h1 className="text-lg font-semibold text-white">Delba</h1>
+                <p className="mt-2 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi, debitis?
+                </p>
+                <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+                  View Profile &rarr;
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className='transition-transform transform hover:scale-105'>
+          <div className=" boxes h-[84vh] w-[100%] transition-transform transform hover:scale-105">
+              <img
+                src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+                alt="AirMax Pro"
+                className="z-0 h-[100%] w-[100%] object-cover"
+              />
+              <div className="absolute w-[100%] inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+              <div className="absolute bottom-4 left-4 text-left">
+                <h1 className="text-lg font-semibold text-white">Delba</h1>
+                <p className="mt-2 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi, debitis?
+                </p>
+                <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+                  View Profile &rarr;
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className='transition-transform transform hover:scale-105'>
+          <div className=" boxes h-[84vh] w-[100%] transition-transform transform hover:scale-105">
+              <img
+                src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+                alt="AirMax Pro"
+                className="z-0 h-[100%] w-[100%] object-cover"
+              />
+              <div className="absolute w-[100%] inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+              <div className="absolute bottom-4 left-4 text-left">
+                <h1 className="text-lg font-semibold text-white">Delba</h1>
+                <p className="mt-2 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi, debitis?
+                </p>
+                <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+                  View Profile &rarr;
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className='transition-transform transform hover:scale-105'>
+          <div className=" boxes h-[84vh] w-[100%] transition-transform transform hover:scale-105">
+              <img
+                src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+                alt="AirMax Pro"
+                className="z-0 h-[100%] w-[100%] object-cover"
+              />
+              <div className="absolute w-[100%] inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+              <div className="absolute bottom-4 left-4 text-left">
+                <h1 className="text-lg font-semibold text-white">Delba</h1>
+                <p className="mt-2 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi, debitis?
+                </p>
+                <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+                  View Profile &rarr;
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          
+        </Swiper>
+        {/* <div className="h-24"></div> */}
+      </div>
+    </>
+  );
+}
+
+export default CarousalCards
---
+++ src/Components/Faculty-cards.jsx
@@ -0,0 +1,129 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation, Pagination,Autoplay,Mousewheel} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import "./CSS/Faculty-card.css"
+
+const Facultycards = () => {
+  return (
+    <div className="main-faculty-carousel mb-10">
+      <Swiper
+          spaceBetween={0}
+        slidesPerView={1}
+        // centeredSlides={true}
+        loop={true}
+        // autoplay={{
+        //   delay: 2500,
+        //   // disableOnInteraction: false,
+        // }}
+        cssMode={true}
+        sticky={false}
+        freeMode={{ enable: true }}
+        mousewheel={{
+          mousewheel: true,
+        }}
+        navigation={true}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 1,
+          },
+        }}
+        modules={[Autoplay, Pagination, Navigation, Mousewheel]}
+      >
+        <SwiperSlide>
+          <div className="container ml-2">
+            <div className="card">
+              <div className="imgBx">
+                <img src="../../Images/ruchi-ma'am.png" />
+              </div>
+              <div className="contentBx">
+                <h2>Assistant Profesor</h2>
+                
+                <div className="color">
+                    <h1>DR. RUCHI GUPTA</h1>
+                </div>
+                  <a href="#">View Profile</a>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="container ml-2">
+            <div className="card">
+              <div className="imgBx">
+                <img src="../../Images/chairman.png" />
+              </div>
+              <div className="contentBx">
+                <h2>Chairman</h2>
+                
+                <div className="color">
+                    <h1>Ashok Pal</h1>
+                </div>
+                  <a href="#">View Profile</a>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="container ml-2">
+            <div className="card">
+              <div className="imgBx">
+                <img src="../../Images/director.png" />
+              </div>
+              <div className="contentBx">
+                <h2>Director General</h2>
+                
+                <div className="color">
+                    <h1>DR. R.K Agrawal</h1>
+                </div>
+                  <a href="#">View Profile</a>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="container ml-2">
+            <div className="card">
+              <div className="imgBx">
+                <img src="../../Images/sarvachan-sir.png" />
+              </div>
+              <div className="contentBx">
+                <h2>Assistant Profesor</h2>
+                
+                <div className="color">
+                    <h1>SARVACHAN VERMA</h1>
+                </div>
+                  <a href="#">View Profile</a>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="container ml-2">
+            <div className="card">
+              <div className="imgBx">
+                <img src="../../Images/ruchi-ma'am.png" />
+              </div>
+              <div className="contentBx">
+                <h2>Assistant Profesor</h2>
+                
+                <div className="color">
+                    <h1>DR. RUCHI GUPTA</h1>
+                </div>
+                  <a href="#">View Profile</a>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+export default Facultycards
