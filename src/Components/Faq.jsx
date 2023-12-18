b7b636e0965b6871c337799a03f9719405506841
krish <sharmageeta847@gmail.com>
2023-12-19 01:32:12 +0530

    faq added

--- src/Components/Faq.jsx
+++ src/Components/Faq.jsx
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
 import { ChevronUp, ChevronDown } from 'lucide-react';
 
+const faqData = [
+  {
+    question: 'How will I get admission confirmation?',
+    answer: 'Upon successful review of your application, the university will typically send you an official admission offer through email or mail.',
+  },
+  {
+    question: 'Are there English language proficiency requirements?',
+    answer: 'Yes, many universities have English language proficiency requirements for international applicants.',
+  },
+  {
+    question: 'What are the admission requirements?',
+    answer: 'Admission requirements typically include a completed application form and submission of academic transcripts, standardized test scores, and letters of recommendation.',
+  },
+  {
+    question: 'How do I apply to the university?',
+    answer: 'To apply to the university, visit the official admissions page on the university website. Follow the application instructions, complete the online application form, and submit the required documents before the specified deadline.',
+  },
+];
 
 function Faq() {
-    const [faqOpen, setFaqOpen] = useState([]);
+  const [faqOpen, setFaqOpen] = useState([]);
 
   const toggleFaq = (index) => {
     setFaqOpen((prev) => {
@@ -13,60 +31,53 @@ function Faq() {
     });
   };
 
-    return (
-        <section className="mx-auto max-w-7xl mt-12 px-4 py-10 md:px-0">
-          <div>
-            <div className="mx-auto max-w-2xl lg:text-center">
-              <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
-                Frequently Asked Questions
-              </h2>
-              <p className="mt-4 max-w-xl text-base leading-relaxed text-gray-600 lg:mx-auto">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere, assumenda
-              </p>
-            </div>
-            <div className="mx-auto mt-8 max-w-3xl space-y-4 md:mt-16">
-              {Array.from({ length: 4 }).map((_, i) => (
-                <div
-                  key={i}
-                  className="cursor-pointer rounded-md border border-gray-400 transition-all duration-400"
-                >
-                  <button
-                    type="button"
-                    className="flex w-full items-start justify-between px-4 py-5 sm:p-6 md:items-center"
-                    onClick={() => toggleFaq(i)}
-                  >
-                    <span className="flex text-start text-lg font-semibold text-black">
-                    How Will I Get Admission Confirmation?
-                    </span>
-                    {faqOpen[i] ? (
-                      <ChevronUp className="h-5 w-5 text-gray-500 md:block" />
-                    ) : (
-                      <ChevronDown className="h-5 w-5 text-gray-500 md:block" />
-                    )}
-                  </button>
-                  {faqOpen[i] && (
-                    <div className="px-4 pb-5 sm:px-6 sm:pb-6">
-                      <p className="text-gray-500">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aliquam adipisci
-                        iusto aperiam? Sint asperiores sequi nobis inventore ratione deleniti?
-                      </p>
-                    </div>
-                  )}
+  return (
+    <section className="mx-auto max-w-7xl mt-12 px-4 py-10 md:px-0">
+      <div>
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+            Frequently Asked Questions
+          </h2>
+        </div>
+        <div className="mx-auto mt-8 max-w-3xl space-y-4 md:mt-16">
+          {faqData.map((faq, i) => (
+            <div
+              key={i}
+              className="cursor-pointer rounded-md border border-gray-400 transition-all duration-400"
+            >
+              <button
+                type="button"
+                className="flex w-full items-start justify-between px-4 py-5 sm:p-6 md:items-center"
+                onClick={() => toggleFaq(i)}
+              >
+                <span className="flex text-start text-lg font-semibold text-black">
+                  {faq.question}
+                </span>
+                {faqOpen[i] ? (
+                  <ChevronUp className="h-5 w-5 text-gray-500 md:block" />
+                ) : (
+                  <ChevronDown className="h-5 w-5 text-gray-500 md:block" />
+                )}
+              </button>
+              {faqOpen[i] && (
+                <div className="px-4 pb-5 sm:px-6 sm:pb-6">
+                  <p className="text-gray-500">
+                    {faq.answer}
+                  </p>
                 </div>
-              ))}
+              )}
             </div>
-            <p className="textbase mt-6 text-center text-gray-600">
-              Can&apos;t find what you&apos;re looking for?{' '}
-              <a href="#" title="" className="font-semibold text-black hover:underline">
-                Contact our support
-              </a>
-            </p>
-          </div>
-        </section>
-
-);
+          ))}
+        </div>
+        <p className="textbase mt-6 text-center text-gray-600">
+          Can't find what you're looking for?{' '}
+          <a href="#" title="" className="font-semibold text-black hover:underline">
+            Contact our support
+          </a>
+        </p>
+      </div>
+    </section>
+  );
 }
 
-export default Faq
-
-
+export default Faq;
--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -3,212 +3,217 @@ import React from 'react'
 
 function Features() {
     return (
-        <div className="mx-auto max-w-7xl px-8 lg:px-12  ">
-         {/* <h1 className='text-4xl ml-5 mb-8 font-sans'>Why choose AKGU ?</h1> */}
-         <h1 className='mb-[5vh] text-4xl font-bold'>Why choose AKGU ?</h1>
-        <hr  className='mb-[5vh] w-[100%]'/>
-          <div className="grid grid-cols-1 gap-y-8 text-center sm:grid-cols-2 sm:gap-12 lg:grid-cols-4 pb-12">
-            <div>
-              <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-blue-100">
-                <svg
-                  className="h-9 w-9 text-blue-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-                  />
-                </svg>
-              </div>
-              <h3 className="mt-8 text-lg font-semibold text-black">Diverse Learning Mediums</h3>
-              <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-                consequat duis enim velit mollit.
-              </p>
+      <div className="mx-auto max-w-7xl px-8 lg:px-12  ">
+        {/* <h1 className='text-4xl ml-5 mb-8 font-sans'>Why choose AKGU ?</h1> */}
+        <h1 className="mb-[5vh] text-4xl font-bold">Why choose AKGU ?</h1>
+        <hr className="mb-[5vh] w-[100%]" />
+        <div className="grid grid-cols-1 gap-y-8 text-center sm:grid-cols-2 sm:gap-12 lg:grid-cols-4 pb-12">
+          <div>
+            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-blue-100">
+              <svg
+                className="h-9 w-9 text-blue-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
+                />
+              </svg>
             </div>
-            <div>
-              <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-orange-100">
-                <svg
-                  className="h-9 w-9 text-orange-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M13 10V3L4 14h7v7l9-11h-7z"
-                  />
-                </svg>
-              </div>
-              <h3 className="mt-8 text-lg font-semibold text-black">Dedicated Academic Advisor</h3>
-              <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-                consequat duis enim velit mollit.
-              </p>
-            </div>
-            <div>
-              <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-green-100">
-                <svg
-                  className="h-9 w-9 text-green-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                  />
-                </svg>
-              </div>
-              <h3 className="mt-8 text-lg font-semibold text-black">AI-Professor AMI</h3>
-              <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-                consequat duis enim velit mollit.
-              </p>
-            </div>
-            <div>
-              <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-red-100">
-                <svg
-                  className="h-9 w-9 text-red-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
-                  />
-                </svg>
-              </div>
-              <h3 className="mt-8 text-lg font-semibold text-black">Career Services</h3>
-              <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-                consequat duis enim velit mollit.
-              </p>
-            </div>
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              Academic Reputation
+            </h3>
+            <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
+              university's overall academic reputation and rankings in your
+              field of study.
+            </p>
           </div>
-
-
-
-          <div className="grid grid-cols-1 gap-y-8 text-center sm:grid-cols-2 sm:gap-12 lg:grid-cols-4">
-            <div>
-              <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-blue-100">
-                <svg
-                  className="h-9 w-9 text-blue-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-                  />
-                </svg>
-              </div>
-              <h3 className="mt-8 text-lg font-semibold text-black">Doorstep Delivery Of Books</h3>
-              <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-                consequat duis enim velit mollit.
-              </p>
-            </div>
-            <div>
-              <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-orange-100">
+          <div>
+            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-orange-100">
               <svg
-  className="h-9 w-9 text-orange-600"
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="currentColor"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="1.5"
-    d="M21 12a9 9 0 11-18 0 9 9 0 0118 0zM12 3l-4 4-4-4M12 21l4-4 4 4"
-  />
-</svg>
-
-
-              </div>
-              <h3 className="mt-8 text-lg font-semibold text-black">Metaverse Campus</h3>
-              <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-                consequat duis enim velit mollit.
-              </p>
+                className="h-9 w-9 text-orange-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M13 10V3L4 14h7v7l9-11h-7z"
+                />
+              </svg>
             </div>
-            <div>
-              <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-green-100">
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              Facilities and Resources
+            </h3>
+            <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
+              Evaluate the campus facilities, libraries, laboratories, and
+              technology resources available to students.
+            </p>
+          </div>
+          <div>
+            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-green-100">
               <svg
-  className="h-9 w-9 text-green-600"
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="currentColor"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="1.5"
-    d="M21 12a9 9 0 11-18 0 9 9 0 0118 0zM12 6v6l3-3m-6 0l3 3m0 0l3-3m-3 3v6"
-  />
-</svg>
-
-
-              </div>
-              <h3 className="mt-8 text-lg font-semibold text-black">Offline Experiences</h3>
-              <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-                consequat duis enim velit mollit.
-              </p>
+                className="h-9 w-9 text-green-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              </svg>
             </div>
-            <div>
-              <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-red-100">
+            <h3 className="mt-8 text-lg font-semibold text-black">Faculty</h3>
+            <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
+              Research the qualifications and expertise of the faculty in your
+              department of interest.
+            </p>
+          </div>
+          <div>
+            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-red-100">
               <svg
-  className="h-9 w-9 text-red-600"
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="currentColor"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="1.5"
-    d="M5 5h14M5 12h14m-14 7h14"
-  />
-  <circle cx="12" cy="12" r="2" fill="currentColor" />
-</svg>
-
-
-              </div>
-              <h3 className="mt-8 text-lg font-semibold text-black">Placement Opportunities</h3>
-              <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-                consequat duis enim velit mollit.
-              </p>
+                className="h-9 w-9 text-red-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
+                />
+              </svg>
             </div>
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              Career Services
+            </h3>
+            <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
+              Look into the career services offered by the university, including
+              internships, job placement, and alumni networks.
+            </p>
           </div>
         </div>
 
-        
-  )
+        <div className="grid grid-cols-1 gap-y-8 text-center sm:grid-cols-2 sm:gap-12 lg:grid-cols-4">
+          <div>
+            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-blue-100">
+              <svg
+                className="h-9 w-9 text-blue-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
+                />
+              </svg>
+            </div>
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              Alumni Success
+            </h3>
+            <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
+              Research the achievements of the university's alumni to gauge the
+              institution's impact on graduates' careers.
+            </p>
+          </div>
+          <div>
+            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-orange-100">
+              <svg
+                className="h-9 w-9 text-orange-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0zM12 3l-4 4-4-4M12 21l4-4 4 4"
+                />
+              </svg>
+            </div>
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              Campus Life
+            </h3>
+            <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
+              Explore extracurricular activities, clubs, and organizations to
+              gauge the overall campus experience.
+            </p>
+          </div>
+          <div>
+            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-green-100">
+              <svg
+                className="h-9 w-9 text-green-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0zM12 6v6l3-3m-6 0l3 3m0 0l3-3m-3 3v6"
+                />
+              </svg>
+            </div>
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              Accreditation
+            </h3>
+            <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
+              Ensure the university is accredited, as this ensures academic
+              quality and recognition.
+            </p>
+          </div>
+          <div>
+            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-red-100">
+              <svg
+                className="h-9 w-9 text-red-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M5 5h14M5 12h14m-14 7h14"
+                />
+                <circle cx="12" cy="12" r="2" fill="currentColor" />
+              </svg>
+            </div>
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              {" "}
+              Research Opportunities
+            </h3>
+            <p className="mt-4 px-7 sm:px-0 text-sm text-gray-600">
+              Check for opportunities to gain practical experience through
+              internships and research programs.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Features
