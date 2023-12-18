18da97cb38835f6ee92fa84e362e0fae778e3692
traptijindal <traptijindal5678@gmail.com>
2023-12-18 21:52:22 +0530

    info added

--- src/Components/BachelorCards.jsx
+++ src/Components/BachelorCards.jsx
@@ -50,26 +50,7 @@ function BachelorCards() {
   return ( 
     <div className="  w-5/6 h-auto flex flex-wrap flex-row items-center justify-center bg-customWhite">
 
-<div className="sm:w-3/4 md:w-1/3 lg:w-1/4 w-full p-4  m-3 bg-white">
 
-   
-   <div className="sm:w-3/4 md:w-1/3 lg:w-1/4 w-full p-4 shadow-lg  m-3 bg-white " >
-    <div className="overflow-hidden">
-      <img className="h-[250px]  object-cover  " src="../../Images/pro1.jpg" alt="" />
-      <div className="p-4">
-        <h3 className="text-xl font-bold mb-2 text-customblue">Card Heading 1</h3>
-        <div className="flex items-center text-gray-600 mb-2">
-          <FontAwesomeIcon icon={faGraduationCap} className="mr-2" />
-          <p>Degree: Bachelor</p>
-        </div>
-        <div className="flex items-center text-gray-600 mb-2">
-          <FontAwesomeIcon icon={faClock} className="mr-2" />
-          <p>Duration: 4 years</p>
-        </div>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"   onClick={openPopup1}>Know More</button>
-      </div>
-    </div>
-  </div>
 
   {popupVisible1 && (
           <div className="fixed top-[3%] left-[10%] transform -translate-x-1/2 -translate-y-1/2    z-50">
@@ -209,7 +190,7 @@ function BachelorCards() {
   </div>
    </div>  
  
-  </div> 
+ 
   
 
   )
