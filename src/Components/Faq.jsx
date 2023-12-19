7d932dd7bebcd7c62637637b0345f8ec2195a3d6
krish <sharmageeta847@gmail.com>
2023-12-19 20:00:18 +0530

    faq added

--- src/Components/Faq.jsx
+++ src/Components/Faq.jsx
@@ -21,7 +21,7 @@ const faqData = [
 ];
 
 function Faq() {
-  const [faqOpen, setFaqOpen] = useState([]);
+  const [faqOpen, setFaqOpen] = useState(Array(faqData.length).fill(false));
 
   const toggleFaq = (index) => {
     setFaqOpen((prev) => {
@@ -31,55 +31,53 @@ function Faq() {
     });
   };
 
-    return (
-        <section className="mx-auto max-w-7xl mt-12 px-4 py-10 md:px-0">
-          <div>
-            <div className="mx-auto max-w-3xl lg:text-center">
-              <h2 className="text-3xl font-bold h-24 leading-tight text-black sm:text-4xl lg:text-5xl">
-                Frequently Asked Questions...
-              </h2>
-            </div>
-            <div className="mx-auto mt-8 max-w-3xl space-y-4 md:mt-16">
-              {Array.from({ length: 4 }).map((_, i) => (
-                <div
-                  key={i}
-                  className="cursor-pointer rounded-md border border-gray-400 transition-all duration-400"
-                >
-                  <button
-                    type="button"
-                    className="flex w-full items-start justify-between px-4 py-5 sm:p-6 md:items-center"
-                    onClick={() => toggleFaq(i)}
-                  >
-                    <span className="flex text-start text-lg font-semibold text-black">
-                    How Will I Get Admission Confirmation?
-                    </span>
-                    {faqOpen[i] ? (
-                      <ChevronUp className="h-5 w-5 text-gray-500 md:block" />
-                    ) : (
-                      <ChevronDown className="h-5 w-5 text-gray-500 md:block" />
-                    )}
-                  </button>
-                  {faqOpen[i] && (
-                    <div className="px-4 pb-5 sm:px-6 sm:pb-6">
-                      <p className="text-gray-500">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aliquam adipisci
-                        iusto aperiam? Sint asperiores sequi nobis inventore ratione deleniti?
-                      </p>
-                    </div>
-                  )}
+  return (
+    <section className="mx-auto max-w-7xl mt-12 px-4 py-10 md:px-0">
+      <div>
+        <div className="mx-auto max-w-3xl lg:text-center">
+          <h2 className="text-3xl font-bold h-24 leading-tight text-black sm:text-4xl lg:text-5xl">
+            Frequently Asked Questions...
+          </h2>
+        </div>
+        <div className="mx-auto mt-8 max-w-3xl space-y-4 md:mt-16">
+          {faqData.map((item, i) => (
+            <div
+              key={i}
+              className={`cursor-pointer rounded-md border border-gray-400 transition-all duration-300 ${
+                faqOpen[i] ? 'shadow-md' : ''
+              }`}
+            >
+              <button
+                type="button"
+                className="flex w-full items-start justify-between px-4 py-5 sm:p-6 md:items-center"
+                onClick={() => toggleFaq(i)}
+              >
+                <span className="flex text-start text-lg font-semibold text-black">
+                  {item.question}
+                </span>
+                {faqOpen[i] ? (
+                  <ChevronUp className="h-5 w-5 text-gray-500 md:block" />
+                ) : (
+                  <ChevronDown className="h-5 w-5 text-gray-500 md:block" />
+                )}
+              </button>
+              {faqOpen[i] && (
+                <div className="px-4 pb-5 sm:px-6 sm:pb-6">
+                  <p className="text-gray-500">{item.answer}</p>
                 </div>
-              ))}
+              )}
             </div>
-            <p className="textbase mt-6 text-center text-gray-600">
-              Can&apos;t find what you&apos;re looking for?{' '}
-              <a href="#" title="" className="font-semibold text-black hover:underline">
-                Contact our support
-              </a>
-            </p>
-          </div>
-        </section>
-
-);
+          ))}
+        </div>
+        <p className="text-base mt-6 text-center text-gray-600">
+          Can&apos;t find what you&apos;re looking for?{' '}
+          <a href="#" title="" className="font-semibold text-black hover:underline">
+            Contact our support
+          </a>
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default Faq;
