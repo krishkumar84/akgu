d9f3abf249305ff6b40509fb3675fb417ac7e042
krish <sharmageeta847@gmail.com>
2023-12-20 00:10:49 +0530

    contact page updated and navbarbtn

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -61,12 +61,13 @@ export default function Navbar() {
                    <Link to="/contact"><li className="text-sm font-semibold text-gray-800 hover:text-gray-900">Contact</li></Link>
                   </nav>
                 </div>
+                <Link to='/form'>
                 <button
                   type="button"
                   className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                 >
-                  Get Started
-                </button>
+                  Enroll Now
+                </button></Link>
               </div>
             </div>
           </div>
