c3747d34cc0bac034c17c416f3d23bac782e7f14
krish <sharmageeta847@gmail.com>
2023-12-19 20:14:18 +0530

    data

--- src/Components/Formback.jsx
+++ src/Components/Formback.jsx
@@ -28,7 +28,7 @@ const Formback = () => {
     <div className='p-16  text-base sm:text-lg '>
       <p className='p-2 text-center'>Ajay Kumar Garg Engineering College (AKGEC), Ghaziabad is affiliated to Dr. A.P.J. Abdul Kalam Technical University, Lucknow, and is approved by the All India Council for Technical Education. 
       </p>
-      <p className='p-2 text-center'>The college was established in 1998 and offers B.Tech courses in nine disciplines of Engineeri, Electrical and Electronics Engineering, Mechanical Engineering and Civil Engineering. B.Tech programs in Computer Science.
+      <p className='p-2 text-center'>The college was established in 1998 and offers B.Tech courses in nine disciplines of Engineering, Electrical and Electronics Engineering, Mechanical Engineering and Civil Engineering. B.Tech programs in Computer Science.
       </p>
     </div>
     <div>
