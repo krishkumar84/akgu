3d28c176d72b1572e3194629b4594b20d8d4c23d
traptijindal <traptijindal5678@gmail.com>
2023-12-23 11:32:23 +0530

    change

--- src/Components/PlacementCarousal.jsx
+++ src/Components/PlacementCarousal.jsx
@@ -100,8 +100,7 @@ const PlacementCarousal = () => {
                   </li>
                 </ul>
                 <a href="https://www.akgec.ac.in/placement-highlights/"
-                  
-                  className="rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                className="rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                 >
                   CLOUDERA
                 </a>
