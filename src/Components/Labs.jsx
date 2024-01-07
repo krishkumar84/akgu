91e865187bf6e0840cd15314d93dadc346b8e2e2
traptijindal <traptijindal5678@gmail.com>
2024-01-07 17:24:18 +0530

    labs

--- src/Components/Labs.jsx
+++ src/Components/Labs.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Cards_labs from './Cards_labs'
+import Labs_heading from './Labs_heading'
+import Labs_dropdown from './Labs_dropdown'
 
 const Labs = () => {
   return (
-    <div>Labs</div>
+   <>
+   <Labs_heading/>
+   <Labs_dropdown/>
+   <Cards_labs/>
+  
+   </>
   )
 }
 
---
+++ src/Components/Labs_heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Labs_heading() {
+  return (
+    <div className="flex flex-wrap w-full h-[100px] sm:h-[150px] items-center justify-center bg-slate-700">
+      <p className=' text-3xl sm:text-4xl text-white font-normal '>IT Labs</p>
+    </div>
+  )
+}
+
+export default Labs_heading
